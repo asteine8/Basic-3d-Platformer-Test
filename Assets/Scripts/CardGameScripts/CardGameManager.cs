9ccc6f7ab785f2c26135d72ab535a3ae15bc557f
Alexandra Steiner <asteine8@gmail.com>
2025-04-05 13:24:32 -0700

    Added card Game stuff

---
+++ Assets/Scripts/CardGameScripts/CardGameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardGameManager : MonoBehaviour
+{
+
+    public GameObject PlayerObject;
+    public GameObject EnemyObject;
+
+    public List<GameObject> CardLocations;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
